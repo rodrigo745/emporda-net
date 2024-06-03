4593726b2320c27bcd60a2acff58c3617e1b61b3
rodrigo745 <roodrigoquinteros@gmail.com>
2024-06-03 05:14:19 -0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 // Componentes
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
+import MainUno from "@/components/MainUno";
+
 
 export default function Home() {
   return (
     <main className="">
       <NavBar/>
       <Header/>
+      <hr/>
+      <MainUno/>
+
     </main>
   );
 }
---
+++ src/components/MainUno.jsx
@@ -0,0 +1,7 @@
+export default function MainUno(){
+    return(
+        <div >
+            Main UNo
+        </div>
+    )
+}
